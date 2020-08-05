675c1e2dd7283490e6000f08fcdee9dfff7c10d5
Sergio <figueroa.sergio.26@gmail.com>
2020-08-05 18:11:46 -0500

    avance visual

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() {
+    if ($("#menu").offset().top > 56) {
+        $("#menu").css({'background':'#25c768'});
+        $("#menu").removeClass('navbar-dark');
+        $("#menu").addClass('navbar-light');
+    } else {
+        $("#menu").css({'background':'transparent'});
+        $("#menu").removeClass('navbar-light');
+        $("#menu").addClass('navbar-dark');
+    }
+  });
