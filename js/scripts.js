8098a670dc4aa7cce93b6a7e4d652bed16f94187
Sergio <figueroa.sergio.26@gmail.com>
2020-08-06 19:35:54 -0500

    random button athe other pages

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,35 @@
         $("#menu").addClass('navbar-dark');
     }
   });*/
+function generateRandomNumber (min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+function randomButton() {
+
+    var boton = document.getElementById("randomButton")
+    switch (generateRandomNumber(1, 4)) {
+        case 1:
+            boton.innerHTML = "3D models";
+            boton.href="/3DModels.html"
+            break;
+        case 2:
+            boton.innerHTML= "Android";
+            boton.href="/resumen.html";
+            break;
+        case 3:
+            boton.innerHTML= "Fotos";
+            boton.href="/fotos.html"
+            break;
+        case 4:
+            boton.innerHTML = "Sobre mi";
+            boton.href="/resumen.html"
+            break;
+
+    }
+}
+//se ejecuta cuando se carga el html
+onload = function () {
+    randomButton();
+    //se da un intervalo para ejecutar la funcion
+    setInterval(randomButton, 2000);
+}
